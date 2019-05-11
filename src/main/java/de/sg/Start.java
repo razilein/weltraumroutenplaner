74414caac48ef6a0a83d2a099da3ef8d4b7cbe7f
razilein <soul_reaverfan@hotmail.de>
2019-05-11 20:45:44 +0200

    Benutzereingabe ermöglichen

--- src/main/java/de/sg/Start.java
+++ src/main/java/de/sg/Start.java
@@ -1,6 +1,8 @@
 package de.sg;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import de.sg.model.Graph;
 import de.sg.model.Route;
@@ -12,31 +14,33 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Start {
 
-    private static final String STARTPLANET = "Erde";
-
-    private static final String ZIELPLANET = "b3-r7-r4nd7";
-
     public static void main(final String[] args) {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         try {
             final Graph graphs = GraphUtils.getGraphs();
-            final int start = GraphUtils.getIndexOfGraphByLabel(graphs.getNodes(), STARTPLANET);
+            final int start = readEingabe(br, graphs, "Startplaneten");
+            final int ziel = readEingabe(br, graphs, "Zielplaneten");
 
-            if (start == -1) {
-                throw new IllegalStateException("Der angebene Startplanet '" + STARTPLANET + "' kann nicht gefunden werden.");
-            }
-
-            final int ziel = GraphUtils.getIndexOfGraphByLabel(graphs.getNodes(), ZIELPLANET);
-            if (ziel == -1) {
-                throw new IllegalStateException("Der angebene Zielplanet '" + STARTPLANET + "' kann nicht gefunden werden.");
-            }
-
-            log.info("Bitte haben Sie einen Moment Geduld. Die schnellste Route von Planet '{}' nach Planet '{}' wird berechnet.",
-                    STARTPLANET, ZIELPLANET);
+            log.info("Bitte haben Sie einen Moment Geduld. Die schnellste Route wird berechnet.");
             final Route route = BerechneRouteUtils.findeKuerzesteRoute(graphs, start, ziel);
             ZeigeRouteUtils.anzeigen(start, route, graphs.getNodes());
         } catch (final IOException e) {
-            log.error("Die Route kann nicht berchnet werden! Sie sind auf sich selbst angewiesen! Viel Glück! ", e.getMessage(), e);
+            log.error("Die Route kann nicht berechnet werden! Sie sind auf sich selbst angewiesen! Viel Glück! ", e.getMessage(), e);
         }
+    }
+
+    private static int readEingabe(final BufferedReader br, final Graph graphs, final String bezeichnung) throws IOException {
+        String eingabePlanet = "";
+        int index = -1;
+        while (eingabePlanet.length() == 0 || index == -1) {
+            log.info("Bitte geben Sie den Namen des {} an: ", bezeichnung);
+            eingabePlanet = br.readLine();
+            index = GraphUtils.getIndexOfGraphByLabel(graphs.getNodes(), eingabePlanet);
+            if (index == -1) {
+                log.info("Der angebene Planet '" + eingabePlanet + "' kann nicht gefunden werden.");
+            }
+        }
+        return index;
     }
 
 }
